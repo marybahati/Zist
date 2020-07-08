5d473d46bba12eec184b75e6504f029ea323e2e7
gracekimaniwairimu <gracewairimukim@gmail.com>
2020-07-08 19:42:58 +0300

    fix location state transfer and add modals to the shopping page

--- src/components/Vendor/VendorRegistration.jsx
+++ src/components/Vendor/VendorRegistration.jsx
@@ -35,20 +35,53 @@ const FormInput = styled.input`
     font-size: 18px !important;
     width: 100%;
     margin: 20px 0 !important;
+    @media only screen and (min-width: 1400px) {
+        padding: 50px 20px !important;
+        font-size: 29px !important;
+    }
 `;
 const FormDropdowns = styled(Dropdown)`
     padding: 30px 20px !important;
     font-size: 18px !important;
     width: 100%;
     margin: 20px 0 !important;
+    @media only screen and (min-width: 1400px) {
+        padding: 50px 20px !important;
+        font-size: 29px !important;
+    }
 `;
 const FormTitle = styled.h1`
     text-align: center;
 `;
 const FormLabels = styled.label`
     font-size: 20px !important;
+    
 `;
-
+const countryOptions = [
+    { key: 'af', value: 'af', flag: 'af', text: 'Afghanistan' },
+    { key: 'ax', value: 'ax', flag: 'ax', text: 'Aland Islands' },
+    { key: 'al', value: 'al', flag: 'al', text: 'Albania' },
+    { key: 'dz', value: 'dz', flag: 'dz', text: 'Algeria' },
+    { key: 'as', value: 'as', flag: 'as', text: 'American Samoa' },
+    { key: 'ad', value: 'ad', flag: 'ad', text: 'Andorra' },
+    { key: 'ao', value: 'ao', flag: 'ao', text: 'Angola' },
+    { key: 'ai', value: 'ai', flag: 'ai', text: 'Anguilla' },
+    { key: 'ag', value: 'ag', flag: 'ag', text: 'Antigua' },
+    { key: 'ar', value: 'ar', flag: 'ar', text: 'Argentina' },
+    { key: 'am', value: 'am', flag: 'am', text: 'Armenia' },
+    { key: 'aw', value: 'aw', flag: 'aw', text: 'Aruba' },
+    { key: 'au', value: 'au', flag: 'au', text: 'Australia' },
+    { key: 'at', value: 'at', flag: 'at', text: 'Austria' },
+    { key: 'az', value: 'az', flag: 'az', text: 'Azerbaijan' },
+    { key: 'bs', value: 'bs', flag: 'bs', text: 'Bahamas' },
+    { key: 'bh', value: 'bh', flag: 'bh', text: 'Bahrain' },
+    { key: 'bd', value: 'bd', flag: 'bd', text: 'Bangladesh' },
+    { key: 'bb', value: 'bb', flag: 'bb', text: 'Barbados' },
+    { key: 'by', value: 'by', flag: 'by', text: 'Belarus' },
+    { key: 'be', value: 'be', flag: 'be', text: 'Belgium' },
+    { key: 'bz', value: 'bz', flag: 'bz', text: 'Belize' },
+    { key: 'bj', value: 'bj', flag: 'bj', text: 'Benin' },
+  ]
 export const VendorRegistration = (props) => {
     return (
         <div>
@@ -70,7 +103,7 @@ export const VendorRegistration = (props) => {
             </Intro>
             <MainGrid>
                 <Grid.Row width={16} >
-                    <Grid.Column width={13}>
+                    <Grid.Column width={14}>
                         <FormTitle>Enter your business information.</FormTitle>
                         <Form >
                             <Form.Field>
@@ -86,7 +119,7 @@ export const VendorRegistration = (props) => {
                                 fluid
                                 search
                                 selection
-                                // options={countryOptions}
+                                options={countryOptions}
                             />
                             </Form.Field>
 
@@ -97,7 +130,7 @@ export const VendorRegistration = (props) => {
                                 fluid
                                 search
                                 selection
-                                // options={countryOptions}
+                                options={countryOptions}
                             />
                             </Form.Field>
                             
@@ -113,7 +146,7 @@ export const VendorRegistration = (props) => {
 
                             </Form>
                     </Grid.Column>
-                    <Icons width={3}>
+                    <Icons width={2}>
                         <Icon name='help circle' size='huge' />
                     </Icons>
                 </Grid.Row>
