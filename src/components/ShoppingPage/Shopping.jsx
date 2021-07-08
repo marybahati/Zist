37783100b34c7737cd0c56f99a79d7d9ba8e3ead
marybahati <bahatimary745@gmail.com>
2021-07-08 18:50:45 +0300

    vendor reg form changes

--- src/components/ShoppingPage/Shopping.jsx
+++ src/components/ShoppingPage/Shopping.jsx
@@ -122,8 +122,6 @@ const Shopping = (props) => {
   let headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': '*',
-    // 'Access-Control-Allow-Credentials': true,
   }
 
   const fetchBusinesses = () => {
